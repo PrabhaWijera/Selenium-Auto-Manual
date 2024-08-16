1f4b95002e3faeabf03b08ed0ace4b96bb977e5f
PrabhaWijera <prabhashwijerathna2001@gmail.com>
2024-08-16 23:13:43 +0530

    pass the test

--- EnvironmentSetupSelenium-Auto-Maven/Auto-Maven/src/main/java/org/example/Main.java
+++ EnvironmentSetupSelenium-Auto-Maven/Auto-Maven/src/main/java/org/example/Main.java
@@ -20,7 +20,22 @@ public class Main {
         WebDriver driver =new ChromeDriver();
 
         // 2] Open URL https://demo.Opencart.com/
+        // pass the url get parameter
+        driver.get("https://demo.opencart.com/");
 
-        
+
+        // 3]  Validate title should be "Your store"
+        //capture the actual title
+       String act_title = driver.getTitle();
+
+       if(act_title.equals("Your Store")){
+           System.out.println("Test Passed 🟩");
+       }else{
+           System.out.println("Test Failed 🔴");
+       }
+
+        //4] Close browser
+      //  driver.close(); || driver.quit();
+        driver.quit();
     }
 }
