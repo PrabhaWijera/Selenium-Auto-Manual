e5fc7119ba5f59a03056793df3bf76a2ba753798
PrabhaWijera <prabhashwijerathna2001@gmail.com>
2024-08-16 22:46:43 +0530

    first commit

---
+++ EnvironmentSetupSelenium-Auto-Maven/Auto-Maven/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+ Test Case 01
+ ---------
+ 1] Launch browser (chorme)
+ 2] Open URL https://demo.Opencart.com/
+ 3]  Validate title should be "Your store"
+ 4] Close browser
+ */
+public class Main {
+    public static void main(String[] args) {
+
+      //  1] Launch browser (chorme)
+       // ChromeDriver driver=new ChromeDriver();
+
+        WebDriver driver =new ChromeDriver();
+
+        // 2] Open URL https://demo.Opencart.com/
+
+        
+    }
+}
